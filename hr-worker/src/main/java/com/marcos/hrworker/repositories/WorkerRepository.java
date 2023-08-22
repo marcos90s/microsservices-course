fbbe5d48af3f6546538464f3ab61ae97f453cbf7
Marcos Antônio <marcosabes@hotmail.com>
2023-08-22 11:58:08 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/marcos/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.marcos.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcos.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_In_come) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_In_come) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_In_come) VALUES ('Alex', 250.0);
