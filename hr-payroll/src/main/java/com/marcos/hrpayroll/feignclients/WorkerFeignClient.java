93f5748595fc84db123eda1ee1886ae840739e39
Marcos Antônio <marcosabes@hotmail.com>
2023-08-24 19:55:14 -0300

    Ribbon Load balancing

--- hr-payroll/src/main/java/com/marcos/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/marcos/hrpayroll/feignclients/WorkerFeignClient.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.marcos.hrpayroll.entities.Worker;
 
+
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker",path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value ="/{id}")
