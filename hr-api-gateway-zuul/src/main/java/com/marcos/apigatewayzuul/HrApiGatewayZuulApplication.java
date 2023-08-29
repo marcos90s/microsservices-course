1d804a857786470a155fbf4582d5abcbad51b33f
Marcos Antônio <marcosabes@hotmail.com>
2023-08-29 19:21:24 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/main/java/com/marcos/apigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
